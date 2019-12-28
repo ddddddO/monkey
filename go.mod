5e88469a7b284b3716ff35695ba5674da34e91a5
ddddddO <lbfdeatq@gmail.com>
2019-12-28 18:03:24 +0900

    add 字句解析器

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/ddddddO/monkey
+
+go 1.13
---
+++ token/token.go
@@ -0,0 +1,47 @@
+package token
+
+type TokenType string
+
+type Token struct {
+	Type    TokenType
+	Literal string
+}
+
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
+
+	// 識別子＋リテラル
+	IDENT = "IDENT" // add, foo, x, y, ...
+	INT   = "INT"   // 1212
+
+	// 演算子
+	ASSIGN = "="
+	PLUS   = "+"
+
+	// デリミタ
+	COMMA     = ","
+	SEMICOLON = ";"
+
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
+
+	// キーワード
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
+)
+
+var keywords = map[string]TokenType{
+	"fn":  FUNCTION,
+	"let": LET,
+}
+
+// ただの識別子かキーワードか
+func LookupIdent(ident string) TokenType {
+	if tok, ok := keywords[ident]; ok {
+		return tok
+	}
+	return IDENT
+}
